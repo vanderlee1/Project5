71c09a52a10ddfdb8a4d1bc6dd1bd20a7f9b9252
vanderlee1 <69871267+vanderlee1@users.noreply.github.com>
2020-09-24 20:26:08 -0500

    ghkh

--- Project5/Source.cpp
+++ Project5/Source.cpp
@@ -5,8 +5,8 @@
 #include <cstdlib>
 using namespace std;
 
-float meanFunction(int, int,int,int);
-float stdFunction(int,int,int,int, float);
+float meanFunction(int, int,int,int); //prototype for the mean functinon
+float stdFunction(int,int,int,int, float); // prototype for the population standard deviation  function
 
 
 int main()
@@ -21,7 +21,7 @@ int main()
 	ofstream outputFileName;
 
 
-	//local variables
+	//open files
 	inputFileName.open("inMeanStd.dat");
 	outputFileName.open("outMeanStd.dat");
 
@@ -30,37 +30,45 @@ int main()
 	//read values
 	inputFileName >> value1 >> value2 >> value3 >> value4;
 
+	//prompt for the user
+	cout << "This program is going to caluculate the mean and the population standard deviation. \n";
+	cout << "First, the program is going to calculate the mean and the population standard deviation of 4 integers from a document \n";
+	cout << "Second, the program is gonna ask you to put in 4 integers. The program will calculate the mean and the \nstandard deviation of those integers."<< endl << endl << endl;
+	
 	//calculate values
-
-	outputFileName << "This is mean: "<< meanFunction(value1, value2, value3, value4) << endl;
-	outputFileName << "This is standard: " << stdFunction(value1, value2, value3, value4, meanFunction(value1, value2, value3, value4)) << endl;
+	outputFileName << "This is the mean of the integers in the document: "<< meanFunction(value1, value2, value3, value4) << endl;
+	outputFileName << "This is the population standard deviation of the integers in the document: " << stdFunction(value1, value2, value3, value4, meanFunction(value1, value2, value3, value4)) << endl << endl<<endl;
 
 	//output values on the screen
-	cout << "This is mean:  " << meanFunction(value1, value2, value3, value4) << endl;
-	cout << "This is standard:  " << stdFunction(value1, value2, value3, value4, meanFunction(value1, value2, value3, value4)) << endl<<endl;
+	cout << "This is the mean of the integers in the document:  " << meanFunction(value1, value2, value3, value4) << endl;
+	cout << "This is the population standard deviation of the integers in the document:  " << stdFunction(value1, value2, value3, value4, meanFunction(value1, value2, value3, value4)) << endl<<endl;
 
-
-	cout << "put in 4 integers\n"; 
+	//promt for the user
+	cout << "Put in 4 integers. Afer you put in one integer you have to press enter"<< endl ; 
 	cin >> value1 >> value2 >> value3 >> value4;
-	outputFileName << "This is mean: " << meanFunction(value1, value2, value3, value4) << endl;
-	outputFileName << "This is standard: " << stdFunction(value1, value2, value3, value4, meanFunction(value1, value2, value3, value4)) << endl;
-	cout << "This is mean:  " << meanFunction(value1, value2, value3, value4) << endl;
-	cout << "This is standard:  " << stdFunction(value1, value2, value3, value4, meanFunction(value1, value2, value3, value4)) << endl;
+	
+	//puts the calculated values in the document
+	cout << endl;
+	outputFileName << "This is the mean of the integers you filled in:  " << meanFunction(value1, value2, value3, value4) << endl; 
+	outputFileName << "This is the population standard deviation of the integers you filled in: " << stdFunction(value1, value2, value3, value4, meanFunction(value1, value2, value3, value4)) << endl;
+	
+	//Outputs the calculated values on the screen
+	cout << "This is the mean of the integers you filled in: " << meanFunction(value1, value2, value3, value4)<< endl;
+	cout << "This is the population standard deviation of the integers you filled in: " << stdFunction(value1, value2, value3, value4, meanFunction(value1, value2, value3, value4)) << endl;
 
-
+	//close the files
 	inputFileName.close();
 	outputFileName.close();
-
-
+	
 	return 0;
 }
-
-float meanFunction(int value1, int value2, int value3, int value4)
+ 
+float meanFunction(int value1, int value2, int value3, int value4) //Function defintion of the mean function
 {
 	return (float(value1 + value2 + value3 + value4) / float(4));
 }
 
-float stdFunction(int value1, int value2, int value3, int value4, float average)
+float stdFunction(int value1, int value2, int value3, int value4, float average) //Function defintion of the population standard deviation
 {
 	return sqrt((float(pow((value1 - average), 2) + pow((value2 - average), 2) + pow((value3 - average), 2)) + pow((value4 - average), 2)) / 4);
 }
